12a5881418362ec4fc562f88d851919da0d4328d
Fuad <fuad.abdullazade879@gmail.com>
2023-02-15 02:16:39 +0400

    add start script

--- app.js
+++ app.js
@@ -140,6 +140,7 @@ app.use((err, req, res, next) => {
 })
 
 // App setup
-app.listen(3000, () => {
-    console.log('Serving on PORT 3000')
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Serving on PORT ${port}`)
 })
